68d5613c76cf99735eb3820dca1aad95b9add337
Ivan <41762778+ivanstepachev@users.noreply.github.com>
2024-02-14 21:15:06 -0500

    Create set_xray.sh

---
+++ set_xray.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Создание директории для Xray
+mkdir -p /etc/xray
+
+# Установка Xray
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
+
+# Удаление стандартного конфигурационного файла Xray
+cd /usr/local/etc/xray && rm -f config.json
+
+# Загрузка конфигурационного файла Xray
+curl -o config.json https://raw.githubusercontent.com/ivanstepachev/config_light/main/config.json
+
+# Создавние файлов логгирования
+mkdir -p /var/log/Xray/ && touch /var/log/Xray/access.log && touch /var/log/Xray/error.log
+
+# Создание доступа для Xray к файлам логгирования
+chown -R nobody:nogroup /var/log/Xray/
+
+# Настройка прав доступа файлам логгирования для записи
+chmod 0664 /var/log/Xray/access.log && chmod 0664 /var/log/Xray/error.log
+
+# Удаление стандартного конфигурационного файла sysctl
+cd /etc && rm -f sysctl.conf
+
+# Скачивание оптимизированного конфига сетевых настроек
+curl -o /etc/sysctl.conf https://raw.githubusercontent.com/ivanstepachev/config/main/sysctl.conf
+
+# Внесение изменений в настройки
+sysctl -p
+
+# Включение и запуск Xray
+systemctl enable xray && systemctl start xray
+
+# Первое выключение
+systemctl stop xray
+
+# Повторное включение
+systemctl start xray
+
+echo "Настройка Xray завершена"

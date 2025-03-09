be5e6860b9d3fb629381f8c9a95ce2c0fef0dff7
Ivan <41762778+ivanstepachev@users.noreply.github.com>
2025-03-09 01:46:33 -0500

    Update logs_saver.sh

--- logs_saver.sh
+++ logs_saver.sh
@@ -35,7 +35,7 @@ if [ -f access.log.gz ]; then
 fi
 
 aws --endpoint-url=https://s3.gis-1.storage.selcloud.ru \
-s3 cp "/var/log/Xray/$archiveFileName" "s3://logs-access/${currentDate}/${archiveFileName}"
+s3 cp "/var/log/Xray/$archiveFileName" "s3://logs-access/${currentDate}/"
 
 # Проверяем статус выполнения предыдущей команды
 if [ "$?" -eq 0 ]; then
